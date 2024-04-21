ab21a1242002227316870dffc7d9d1844fbf4dad
pokemau <rentillosa90@gmail.com>
2024-04-21 10:53:24 +0800

    remove unused

--- src/main/java/com/example/csit228f2_2/HelloApplication.java
+++ src/main/java/com/example/csit228f2_2/HelloApplication.java
@@ -2,25 +2,11 @@ package com.example.csit228f2_2;
 
 import db.SQLConnection;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.StrokeType;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
 
@@ -34,154 +20,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        AnchorPane pnMain = new AnchorPane();
-        GridPane grid = new GridPane();
-        pnMain.getChildren().add(grid);
-        grid.setAlignment(Pos.CENTER);
-        Text sceneTitle = new Text("Welcome to CSIT228");
-        sceneTitle.setStrokeType(StrokeType.CENTERED);
-        sceneTitle.setStrokeWidth(100);
-        sceneTitle.setFill(Paint.valueOf("#325622"));
-        sceneTitle.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 69));
-        grid.add(sceneTitle, 0, 0, 2, 1);
+        SQLConnection.InitTables();
 
-        Label lblUsername = new Label("Username: ");
-        lblUsername.setTextFill(Paint.valueOf("#c251d5"));
-        lblUsername.setFont(Font.font(40));
-        grid.add(lblUsername, 0, 1);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        Scene scene = new Scene(root, 700, 400);
 
-        TextField tfUsername = new TextField();
-        tfUsername.setFont(Font.font(35));
-        grid.add(tfUsername, 1, 1);
-
-        Label lblPassword = new Label("Password: ");
-        lblPassword.setTextFill(Paint.valueOf("#c251d5"));
-        lblPassword.setFont(Font.font(40));
-        grid.add(lblPassword, 0, 2);
-
-        TextField pfPassword = new PasswordField();
-        pfPassword.setFont(Font.font(35));
-        grid.add(pfPassword, 1, 2);
-
-        Button btnShow = new Button("<*>");
-        HBox hbShow = new HBox();
-        hbShow.getChildren().add(btnShow);
-        hbShow.setAlignment(Pos.CENTER);
-        hbShow.setMaxWidth(150);
-        TextField tfPassword = new TextField();
-        tfPassword.setFont(Font.font(35));
-        grid.add(tfPassword, 1, 2);
-        tfPassword.setVisible(false);
-        grid.add(hbShow, 2, 2);
-
-        btnShow.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent actionEvent) {
-                tfPassword.setText(pfPassword.getText());
-                tfPassword.setVisible(true);
-                pfPassword.setVisible(false);
-                grid.add(new Button("Hello"), 4,4);
-            }
-        });
-
-        btnShow.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                tfPassword.setVisible(false);
-                pfPassword.setVisible(true);
-            }
-        });
-
-
-        Button btnSignIn = new Button("Sign In");
-        btnSignIn.setFont(Font.font(45));
-        HBox hbSignIn = new HBox();
-        hbSignIn.getChildren().add(btnSignIn);
-        hbSignIn.setAlignment(Pos.CENTER);
-        grid.add(hbSignIn, 0, 3, 2, 1);
-        final Text actionTarget = new Text("Hi");
-        actionTarget.setFont(Font.font(30));
-        grid.add(actionTarget, 1, 6);
-
-
-        Button btnRegister = new Button("Register");
-        btnRegister.setFont(Font.font(45));
-        HBox hbRegister = new HBox();
-        hbRegister.getChildren().add(btnRegister);
-        hbRegister.setAlignment(Pos.CENTER);
-        grid.add(hbRegister, 1, 4, 5, 1);
-
-        btnRegister.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                String username = tfUsername.getText();
-                String password = pfPassword.getText();
-
-                if (username.isEmpty() || password.isEmpty()) { return; }
-
-                if (SQLConnection.checkIfUsernameIsUnique(username)) {
-                    SQLConnection.createUser(username, password);
-
-                    actionTarget.setText("CREATED USER SUCCESSFULLY");
-                    actionTarget.setOpacity(1);
-                } else {
-                    actionTarget.setText("USERNAME IS NOT UNIQUE");
-                    actionTarget.setOpacity(1);
-                }
-            }
-        });
-
-        btnSignIn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                String username = tfUsername.getText();
-                String password = pfPassword.getText();
-
-                if (username.isEmpty() || password.isEmpty()) { return; }
-
-                if (SQLConnection.getUser(username, password)) {
-                    actionTarget.setText("LOGGING IN");
-                    actionTarget.setOpacity(1);
-
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-                    try {
-                        Scene scene = new Scene(loader.load());
-
-                        actionTarget.setText("");
-                        actionTarget.setOpacity(0);
-                        tfUsername.setText("");
-                        tfPassword.setText("");
-
-
-                        stage.setScene(scene);
-                        stage.show();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    actionTarget.setText("USER DOES NOT EXIST");
-                    actionTarget.setOpacity(1);
-                }
-
-            }
-        });
-
-        EventHandler<KeyEvent> fieldChange = new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent actionEvent) {
-                actionTarget.setOpacity(0);
-            }
-        };
-        tfUsername.setOnKeyTyped(fieldChange);
-        pfPassword.setOnKeyTyped(fieldChange);
-
-
-        Scene scene = new Scene(pnMain, 700, 560);
         stage.setScene(scene);
-
-        mainStage = stage;
-        mainScene = scene;
-
+        stage.setTitle("My App");
+        stage.setResizable(false);
         stage.show();
     }
 }
