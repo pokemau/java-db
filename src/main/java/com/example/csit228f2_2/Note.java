2de544b7854e10094101a84f7462541933098d4d
pokemau <rentillosa90@gmail.com>
2024-04-22 19:25:30 +0800

    add create tables function and getNotes function

---
+++ src/main/java/com/example/csit228f2_2/Note.java
@@ -0,0 +1,13 @@
+package com.example.csit228f2_2;
+
+public class Note {
+    int ID;
+    int userID;
+    String content;
+
+    public Note(int ID, int userID, String content) {
+        this.ID = ID;
+        this.userID = userID;
+        this.content = content;
+    }
+}
